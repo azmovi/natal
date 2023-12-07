1ec616acd75d30ed486ced27e23ba8a3fb431e90
azevedo <azevedoantoniocicero@gmail.com>
2023-12-06 21:45:42 -0300

    setup para amanha

---
+++ q3/d1.py
@@ -0,0 +1,8 @@
+with open('q2/input.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+
+total = 0
+for linha in linhas:
+    print(linha)
+
+print(total)
---
+++ q3/d2.py
@@ -0,0 +1,7 @@
+with open('input.txt', 'r') as arquivo:
+    linhas = arquivo.readlines()
+
+total = 0
+for linha in linhas:
+    print(linha)
+print(total)
